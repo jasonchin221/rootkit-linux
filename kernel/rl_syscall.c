590f7349d53756de751511b2facfead5f5ff98c5
jason <jasonchin221@163.com>
2015-06-28 22:23:54 +0800

    save
    Signed-off-by: jason <jasonchin221@163.com>

--- kernel/rl_syscall.c
+++ kernel/rl_syscall.c
@@ -6,16 +6,29 @@
 #include <linux/sched.h>
 #include <linux/netdevice.h>
 
+#include "rl_syscall.h"
 
 static void *
 rl_sys_call_table_old;
 
+static ulong 
+rl_get_idt_base(void)
+{
+     rl_idt_t   idt_table;
+
+     __asm__ __volatile__("sidt %0":"=m"(idt_table));
+
+     return idt_table.it_base;
+}
+
 int
 rl_get_syscall_table(void)
 {
-    asm("movl $0x8, rl_sys_call_table_old\n\t");
+    ulong   idt_base;
 
-    printk("Syscall table = %p\n", rl_sys_call_table_old);
+    idt_base = rl_get_idt_base();
+
+    printk("idt_base = %x\n", idt_base);
 
     return 0;
 }
--- kernel/rl_syscall.h
+++ kernel/rl_syscall.h
@@ -1,6 +1,13 @@
 #ifndef __RL_SYSCALL_H__
 #define __RL_SYSCALL_H__
 
+struct _rl_idt_t {
+    u16         it_limit;
+    ulong       it_base;
+}__attribute__((packed));
+
+typedef struct _rl_idt_t rl_idt_t;
+
 extern int rl_get_syscall_table(void);
 
 #endif
